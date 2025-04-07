0d9e2ceb93eda0b3953ff15b0a5fb06232e3e82f
Abhishek <chaudharyabhishekmp@gmail.com>
2025-04-07 14:27:53 +0530

    reg completed

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,125 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { signUpSchema } from "@/schema/signUpSchema";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+
+import Link from "next/link";
+import { LucideLoader2 } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+export default function Page() {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: { username: "", email: "", password: "" },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsLoading(true);
+    try {
+      const responce = await axios.post<ApiResponse>("/api/sign-up", data);
+      toast.success(responce.data.message);
+      router.replace("/signin");
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error in signup of user", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+      let errorMessage = axiosError.response?.data.message;
+      toast.error(errorMessage);
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join NextChat
+          </h1>
+          <p className="mb-4">Sign up to start chating</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="username"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={isLoading}>
+              {isLoading ? (
+                <>
+                  {" "}
+                  <LucideLoader2 className="mr-2 h-4 w-4 animate-spin" /> Please
+                  wait
+                </>
+              ) : (
+                "Signup"
+              )}
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Not a member?{" "}
+            <Link href="/signin" className="text-blue-600 hover:text-blue-800">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,30 +1,31 @@
+import "next-auth";
 import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface User {
-    _id: string;
-    username: string;
-    email: string;
-    publicKey: string;
-    friends: mongoose.Types.ObjectId[];
+    _id?: string;
+    username?: string;
+    email?: string;
+    publicKey?: string;
+    friends?: mongoose.Types.ObjectId[];
   }
   interface Session {
     user: {
-      _id: string;
-      username: string;
-      email: string;
-      publicKey: string;
-      friends: mongoose.Types.ObjectId[];
+      _id?: string;
+      username?: string;
+      email?: string;
+      publicKey?: string;
+      friends?: mongoose.Types.ObjectId[];
     } & DefaultSession["user"];
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    _id: string;
-    username: string;
-    email: string;
-    publicKey: string;
-    friends: mongoose.Types.ObjectId[];
+    _id?: string;
+    username?: string;
+    email?: string;
+    publicKey?: string;
+    friends?: mongoose.Types.ObjectId[];
   }
 }
